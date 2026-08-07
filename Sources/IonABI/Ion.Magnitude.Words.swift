17d8ffa5cc709515dfdb89d501a12bc6e771de81
Diana Ma <diana@rarestype.com>
2026-08-07 20:02:07 +0000

    stash fixed width impl

--- Sources/IonABI/Ion.Magnitude.Words.swift
+++ Sources/IonABI/Ion.Magnitude.Words.swift
@@ -19,21 +19,41 @@ extension Ion.Magnitude.Words: RandomAccessCollection {
     }
 }
 extension Ion.Magnitude.Words {
-    @usableFromInline func load<T>(into _: T.Type = T.self) -> T where T: UnsignedInteger {
-        /// todo: benchmark zero-init and big-endian memory copy
-        if  self.bytes.isEmpty {
-            return .zero
-        }
-        var i: Int = self.bytes.startIndex
-        var value: T = T.init(self.bytes[i])
-        while true {
-            i = self.bytes.index(after: i)
+    @inlinable public func load<T>(
+        into _: T.Type = T.self
+    ) -> T where T: UnsignedInteger & FixedWidthInteger {
+        return self.bytes.withUnsafeBytes {
+            guard
+            let first: UnsafeRawPointer = $0.baseAddress, !$0.isEmpty else {
+                return .zero
+            }
+            let bytes: Int = $0.count
 
-            guard i < self.bytes.endIndex else {
-                return value
+            let length: Int
+            let offset: Int
+            let source: UnsafeRawPointer
+
+            let target: Int = MemoryLayout<T>.size
+            if  target > bytes {
+                // source buffer is smaller than the target, pad with zeroes
+                length = bytes
+                offset = target - bytes
+                source = first
+            } else {
+                // source buffer is larger than the target, truncate
+                // (caller is responsible for bounds checking)
+                length = target
+                offset = 0
+                source = first.advanced(by: bytes - target)
             }
 
-            value = T.init(self.bytes[i]) | value << 8
+            var bigEndian: T = .zero
+
+            withUnsafeMutableBytes(of: &bigEndian) {
+                $0.baseAddress!.advanced(by: offset).copyMemory(from: source, byteCount: length)
+            }
+
+            return T.init(bigEndian: bigEndian)
         }
     }
 }
